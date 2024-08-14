602553003c19595e7e464aac0c056a200a67cb69
AlvinRHD <ezequielhernandes907@gmail.com>
2024-08-14 13:33:43 -0600

    Practica1 Desarrollo de Soft Libre

---
+++ Practica1/index.php
@@ -0,0 +1,32 @@
+<?php
+
+//Cadena
+$nombre = "Alvin";
+$apellido = "Rosales";
+
+//Entero
+$edad = 20;
+
+//Float
+$estatura = 1.70;
+
+//Booleano
+$vivo = true;
+
+//Imprimir en php
+echo $nombre . "<br>";
+print_r($apellido);
+
+echo "<br><br>";
+
+echo $nombre,' ',$apellido,' ',$estatura;
+
+$edad = 'veinticuatro';
+
+var_dump($edad);
+
+echo "<br><br>";
+
+echo 'Mi nombre es ' . $nombre . ' ' . $apellido;
+
+echo "<br><br>";
