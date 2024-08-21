58674003cdd770e06430d6e4d6d0cebf16c9c717
AlvinRHD <ezequielhernandes907@gmail.com>
2024-08-21 12:06:51 -0600

    Practica2 Desarrollo de Soft Libre - Agregando ejercicio 4

--- Practica2/ejercicio4.php
+++ Practica2/ejercicio4.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ejercicio 4</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
@@ -26,3 +26,43 @@
 </body>
 
 </html>
+
+<?php
+
+if (isset($_POST["txtDia"])) {
+
+    $dia = "";
+
+    switch ($_POST["txtDia"]) {
+        case 1:
+            $dia = "Lunes";
+            break;
+        case 2:
+            $dia = "Martes";
+            break;
+        case 3:
+            $dia = "Miercoles";
+            break;
+        case 4:
+            $dia = "Jueves";
+            break;
+        case 5:
+            $dia = "Viernes";
+            break;
+        case 6:
+            $dia = "Sabado";
+            break;
+        case 7:
+            $dia = "Domingo";
+            break;
+
+        default:
+            $dia = "Dia  no existente!";
+            break;
+    }
+
+    echo "<br><br>";
+    echo "Resultado: $dia";
+}
+
+?>
